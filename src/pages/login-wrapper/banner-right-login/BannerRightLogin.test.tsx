5ae89befa95a3a2c342c05ccfd2235477ddc79c0
Renato Veronese - RVR <renato.veronese@iteris.local>
2020-10-07 16:22:24 -0300

    correção dos testes

--- src/pages/login-wrapper/banner-right-login/BannerRightLogin.test.tsx
+++ src/pages/login-wrapper/banner-right-login/BannerRightLogin.test.tsx
@@ -1,71 +1,73 @@
-import { render, fireEvent, wait, act } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import React from "react";
-import BannerRightLogin from "./BannerRightLogin";
-import { Router } from "react-router-dom";
-import { createMemoryHistory } from "history";
-import { SnackbarProvider } from "notistack";
+import { render, fireEvent, act, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import React from 'react';
+import BannerRightLogin from './BannerRightLogin';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { SnackbarProvider } from 'notistack';
 
 const renderWithHistory = () => {
-	const history = createMemoryHistory();
-	const wrapper = render(<SnackbarProvider>
-    <Router history={history}>
-      <BannerRightLogin />
-    </Router>
-  </SnackbarProvider>);
-	return { ...wrapper, history };
+  const history = createMemoryHistory();
+  const wrapper = render(
+    <SnackbarProvider>
+      <Router history={history}>
+        <BannerRightLogin />
+      </Router>
+    </SnackbarProvider>
+  );
+  return { ...wrapper, history };
 };
 
-test("should login user when all fields are filled succesfully", async () => {
-  const {container} = renderWithHistory()
+test('should login user when all fields are filled succesfully', async () => {
+  const { container } = renderWithHistory();
   act(() => {
-    const inputEmail = container.querySelector("#login-user-email")!;
+    const inputEmail = container.querySelector('#login-user-email')!;
     fireEvent.change(inputEmail, {
-      target: { value: "renatop@hotmail.com.br" },
+      target: { value: 'renatop@hotmail.com.br' },
     });
-    const inputPassword = container.querySelector("#login-user-password")!;
+    const inputPassword = container.querySelector('#login-user-password')!;
     fireEvent.change(inputPassword, {
-      target: { value: "123" },
+      target: { value: '123' },
     });
   });
   act(() => {
-    const loginButton = container.querySelector("#login-button")!;
+    const loginButton = container.querySelector('#login-button')!;
     fireEvent.click(loginButton);
   });
-  await wait(() => {
-    expect(localStorage.getItem("token")).toBe("token");
+  await waitFor(() => {
+    expect(localStorage.getItem('token')).toBe('token');
   });
 });
 
 test("should show a label error when click in 'Entrar' button without fill the email", async () => {
-  const {container, getByText} = renderWithHistory()
-  const loginButton = container.querySelector("#login-button")!;
+  const { container, getByText } = renderWithHistory();
+  const loginButton = container.querySelector('#login-button')!;
   fireEvent.click(loginButton);
-  await wait(() => {
-    expect(getByText("Necessário informar o email")).toBeInTheDocument();
+  await waitFor(() => {
+    expect(getByText('Necessário informar o email')).toBeInTheDocument();
   });
 });
 
 test("should show a label error when click in 'Entrar' button without fill the password", async () => {
-  const {container, getByText} = renderWithHistory()
-  const loginButton = container.querySelector("#login-button")!;
+  const { container, getByText } = renderWithHistory();
+  const loginButton = container.querySelector('#login-button')!;
   fireEvent.click(loginButton);
-  await wait(() => {
-    expect(getByText("Necessário informar a senha!")).toBeInTheDocument();
+  await waitFor(() => {
+    expect(getByText('Necessário informar a senha!')).toBeInTheDocument();
   });
 });
 
 test("should show a label error when click in 'Entrar' button without fill a valid email", async () => {
-  const {container, getByText} = renderWithHistory()
+  const { container, getByText } = renderWithHistory();
   act(() => {
-    const inputEmail = container.querySelector("#login-user-email")!;
+    const inputEmail = container.querySelector('#login-user-email')!;
     fireEvent.change(inputEmail, {
-      target: { value: "renatop@" },
+      target: { value: 'renatop@' },
     });
   });
-  const loginButton = container.querySelector("#login-button")!;
+  const loginButton = container.querySelector('#login-button')!;
   fireEvent.click(loginButton);
-  await wait(() => {
-    expect(getByText("Email não possui formato válido")).toBeInTheDocument();
+  await waitFor(() => {
+    expect(getByText('Email não possui formato válido')).toBeInTheDocument();
   });
 });
